5a53511e9bd509a6f71f1eb162bb577abd81cc67
fariz0020 <fariz0020@students.amikom.ac.id>
2019-11-27 20:34:49 +0700

    done pencadangan feature

--- src/app/views/pages/bppencadangan/bppencadangan.module.ts
+++ src/app/views/pages/bppencadangan/bppencadangan.module.ts
@@ -4,13 +4,39 @@ import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 // Core Module
 import {CoreModule} from '../../../core/core.module';
+import { PartialsModule } from '../../partials/partials.module';
 // import { DashboardComponent } from './dashboard.component';
-import {BppencadanganComponent} from '../bppencadangan/bppencadangan.component';
+import {BppencadanganComponent} from './bppencadangan.component';
+import {
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule, MatSnackBarModule,
+    MatSortModule,
+    MatTableModule, MatTabsModule, MatTooltipModule
+} from '@angular/material';
 
 @NgModule({
     imports: [
         CommonModule,
         CoreModule,
+        PartialsModule,
+        MatIconModule,
+        MatButtonModule,
+        MatProgressSpinnerModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
         RouterModule.forChild([
             {
                 path: '',
